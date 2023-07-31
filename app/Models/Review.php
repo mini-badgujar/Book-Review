4eb42b485f6c7ac4379e990707fae48c11ee7799
Damini Badgujar <84827841+mini-badgujar@users.noreply.github.com>
2023-07-31 13:20:06 +0530

    worked on Book Reviews application and understood what components are.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['review', 'rating'];
+    public function book(){
+        return $this->belongsTo(Book::class);
+    }
 }
